ceef36f98ea0e4f8adb56ddbd96b5742075e40b0
tlaukkan <tlaukkan@eb24fc81-24a6-4dc4-93ca-33770fbb9c90>
2009-07-03 09:02:27 +0000

    Started implementing support for running multiple tests in the same runner process serially.

---
+++ VisualNunitRunner/RunnerClient.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO.Pipes;
+
+namespace VisualNunitRunner
+{
+    public class RunnerClient
+    {
+        private string pipeName;
+        private NamedPipeClientStream pipe;
+        
+        private byte[] readBuffer = new byte[65344];
+        private int readByteCount = 0;
+
+        public IList<string> TestCases = new List<string>();
+
+        public RunnerClient(Process serverProcess)
+        {
+            this.pipeName = "VisualNunitRunner-" + Process.GetCurrentProcess().Id;
+            this.pipe = new NamedPipeClientStream(".", pipeName, PipeDirection.InOut, PipeOptions.WriteThrough);
+            this.pipe.Connect(50);
+            this.pipe.ReadMode = PipeTransmissionMode.Message;
+            
+            string testCaseNames="";
+            while (true)
+            {
+                readByteCount = this.pipe.Read(readBuffer, 0, readBuffer.Length);
+                testCaseNames += Encoding.UTF8.GetString(readBuffer, 0, readByteCount);
+                if (this.pipe.IsMessageComplete)
+                {
+                    break;
+                }
+            }
+
+            string[] testCaseNameArray = testCaseNames.Split('\n');
+            foreach (string testCaseName in testCaseNameArray)
+            {
+                TestCases.Add(testCaseName);
+            }
+
+        }
+
+
+
+    }
+}
---
+++ VisualNunitRunner/RunnerServer.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO.Pipes;
+using System.Threading;
+using NUnit.Core;
+using System.IO;
+
+namespace VisualNunitRunner
+{
+
+    /// <summary>
+    /// Server class allowing for running multiple tests in the same process using named pipe for 
+    /// communication with the visual studio process.
+    /// </summary>
+    public class RunnerServer
+    {
+        private string assemblyName;
+        private string pipeName;
+
+        private NamedPipeServerStream pipe;
+
+        private Thread thread;
+        private bool isStopRequested;
+
+        public RunnerServer(string assemblyName)
+        {
+            this.assemblyName = assemblyName;
+            this.pipeName="VisualNunitRunner-"+Process.GetCurrentProcess().Id;
+        }
+
+        /// <summary>
+        /// Start server.
+        /// </summary>
+        public void Start()
+        {
+            // Thread lock to avoid collision with concurrent start invocation or run method exit phase.
+            lock (this)
+            {
+                if (thread != null)
+                {
+                    throw new Exception("Already started.");
+                }
+
+                isStopRequested = false;
+                pipe = new NamedPipeServerStream(pipeName, PipeDirection.InOut, 1, PipeTransmissionMode.Message, PipeOptions.WriteThrough, 65344, 65344);
+                thread = new Thread(Run);
+                thread.Start();
+            }
+        }
+
+        /// <summary>
+        /// Request server stop.
+        /// </summary>
+        public void Stop()
+        {
+            isStopRequested = true;
+        }
+
+        private void Run()
+        {
+            // List the unit tests available to the pipe as single message.
+            ListTests();
+
+            while (!isStopRequested)
+            {
+                // Running up to 50 tests before sleeping.
+                for (int i = 0; i < 50; i++)
+                {
+
+                    // Read pipe for tests to run.
+
+                    if (isStopRequested)
+                    {
+                        break;
+                    }
+                }
+
+                Thread.Sleep(10);
+            }
+
+            // Thread lock to avoid collision with start method invocation.
+            lock (this)
+            {
+                pipe.Disconnect();
+                pipe.Dispose();
+                pipe = null;
+                thread = null;
+            }
+        }
+
+        /// <summary>
+        /// List the unit tests available to the pipe as single message.
+        /// </summary>
+        private void ListTests()
+        {
+            TestSuite testSuite = new TestBuilder().Build(assemblyName, true);
+            Queue<ITest> testQueue = new Queue<ITest>();
+            testQueue.Enqueue(testSuite);
+            String testNames = "";
+            while (testQueue.Count > 0)
+            {
+                ITest test = testQueue.Dequeue();
+                if (test.Tests != null)
+                {
+                    foreach (ITest childTest in test.Tests)
+                    {
+                        testQueue.Enqueue(childTest);
+                    }
+                }
+                else
+                {
+                    testNames += test.TestName.FullName + "\n";
+                }
+            }
+            byte[] testNameBytes = Encoding.UTF8.GetBytes(testNames);
+            pipe.Write(testNameBytes, 0, testNameBytes.Length);
+        }
+
+    }
+}
