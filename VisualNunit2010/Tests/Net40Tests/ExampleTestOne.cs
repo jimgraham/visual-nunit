909993f99e60226598ec5ce2b73e905644094aec
tlaukkan <tlaukkan@eb24fc81-24a6-4dc4-93ca-33770fbb9c90>
2009-11-01 07:45:06 +0000

    Adding tests.

---
+++ VisualNunit2010/Tests/Net40Tests/ExampleTestOne.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace VisualNunitTests
+{
+    [TestFixture]
+    public class ExampleTestOne
+    {
+        [Test]
+        public void TestOneSuccess()
+        {
+        }
+
+        [Test]
+        public void TestOneException()
+        {
+            throw new Exception("Test Exception");
+        }
+
+        [Test]
+        public void TestOneAssert()
+        {
+            Assert.Fail("Test Assert");
+        }
+    }
+}
